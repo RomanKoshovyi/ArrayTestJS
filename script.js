1a69e177034cfc820d894edb7ecc9d73300f1c0c
Roman <romanrokel@gmail.com>
2021-10-09 19:28:34 +0300

    Add all necessary files of LabWork

---
+++ script.js
@@ -0,0 +1,30 @@
+﻿let
+    arr = new Array(), num, min, max, sum,
+    elems = document.getElementById("elems"),
+    size = document.getElementById("size");
+    out = document.getElementById("output");
+
+function Clear(){
+    elems.innerHTML = "";
+    sum = 0;
+}
+
+function Run(){
+    Clear();
+    num = 1*size.value;
+    min = Math.ceil(-1000);
+    max = Math.floor(1000);
+    for (let i = 0; i < num; i++){
+        arr.push(Math.floor(Math.random() * (max - min)) + min);
+    }
+    arr.forEach(element => {
+        elems.innerHTML += (element + ", ");
+    });
+    while (arr.length > 0){
+        let x = 1*arr.splice(0, 1);
+        if (x > 0){
+            sum += x;
+        }
+    }
+    out.innerHTML = "Sum of positive numbers: " + sum;
+}
